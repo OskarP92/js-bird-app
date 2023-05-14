dd9ad30bfdab7c0abea9c3394cd4d91ab52ed80f
OskarP92 <130840888+OskarP92@users.noreply.github.com>
2023-05-14 09:50:10 +0200

    wrapped array in IIFE, updated forEach loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,44 +1,43 @@
-// create array of objects for Pokemon
+let pokemonRepository = (function () {
+    // wrapped listArray in IIFE to avoid accidentally accessing global state
+    let pokemonList = [
+        {
+            name: 'Bulbasaur',
+            height: 0.7,
+            type: ['grass', 'posion'],
+        },
+        {
+            name: 'Charmander',
+            height: 0.6,
+            type: ['fire'],
+        },
+        {
+            name: 'Squirtle',
+            height: 0.5,
+            type: ['water'],
+        }
+    ];
 
-let pokemonList = [
-    {
-        name: 'Bulbasaur',
-        height: 0.7,
-        type: ['grass', 'posion'],
-    },
-    {
-        name: 'Charmander',
-        height: 0.6,
-        type: ['fire'],
-    },
-    {
-        name: 'Squirtle',
-        height: 0.5,
-        type: ['water'],
+    // return object with getAll and add functions assigned as keys
+    function add(pokemon) {
+        pokemonList.push(pokemon);
     }
-];
 
-/*
-// loop that goes through the array starting at 0 index
-for (let i = 0; i < pokemonList.length; i++) {
+    function getAll() {
+        return pokemonList;
+    } 
 
-// write names and heights of Pokemon
-    document.write (pokemonList[i].name + ' (Height: ' + pokemonList[i].height + ')');
+    return {
+        add: add,
+        getAll: getAll
+    };
+})(); 
 
-// conditional for pokemons larger than 0.6
-    if (pokemonList[i].height >0.6) {
+// forEach loop that accesses pokemonList via getAll function 
+pokemonRepository.getAll().forEach(function(pokemon){
+    document.write('<br>' + pokemon.name + ' ' + pokemon.height)
+    if(pokemon.height > 0.6) {
         document.write (' - Wow! That\'s big!');
     }
-
-// add line break between list items
-    document.write('<br>');
-}*/
-
-
-// forEach loop to iterate over pokemonList array with pokemon as a parameter 
-pokemonList.forEach(function(pokemon){
-    document.write('<br>' + pokemon.name + ' ' + pokemon.height)
-        if(pokemon.height > 0.6) {
-            document.write (' - Wow! That\'s big!');
-        }
 });
+
